d3ce0bd9977c5c47ac6374564a5724373b058e0d
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2023-01-08 12:52:21 -0300

    another changes

--- .detoxrc.js
+++ .detoxrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
-      build: 'cd android ; ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug ; cd -',
+      build: 'cd android && ./gradlew assembleDebug && ./gradlew assembleAndroidTest -DtestBuildType=debug && cd ..',
       reversePorts: [
         8081
       ]

0990c81c3a13011d1097118a102cd3c506978167
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2023-01-08 21:00:56 -0300

    add jest-html-reporters lib

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 - ativar workflow ios:
 
 `gh workflow enable e2e_ios.yml`
+
+- ativar workflow android:
+
+`gh workflow enable e2e_android.yml`
--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
-  reporters: ['detox/runners/jest/reporter'],
+  reporters: ['detox/runners/jest/reporter', "jest-html-reporters"],
   testEnvironment: 'detox/runners/jest/testEnvironment',
   verbose: true,
 };

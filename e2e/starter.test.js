9fa87b779b6332038642a48b4634f2ae5a10a629
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2023-01-13 20:03:28 -0300

    last changes for now

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -3,11 +3,14 @@ const { reloadApp } = require('detox-expo-helpers');
 describe('Example', () => {
   beforeEach(async () => {
     await reloadApp();
-    const text = element(by.text('Open up App.tsx to start working on your app!'))
-    await waitFor(text).toBeVisible().withTimeout(10000);
+    await sleep(4000);
   });
 
   it('should have welcome screen', async () => {
     await expect(element(by.text('Open up App.tsx to start working on your app!'))).toBeVisible();
   });
 });
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
